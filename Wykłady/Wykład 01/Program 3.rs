4ae78010cac338b7c3918b35f156e97fead70bad
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-07 19:59:29 +0100

    Update Program 3.rs

--- Wykłady/Wykład 01/Program 3.rs
+++ Wykłady/Wykład 01/Program 3.rs
@@ -1,3 +1,11 @@
+// typy całkowite ze znakiem: i8 i16 i32 i64 i128
+// typy całkowite bez znaku:  u8 u16 u32 u64 u128
+// typy zmiennoprzecinkowe:   f32 f64
+
+// domyślnie: i32 f64
+
+// ścisła zgodność typów!
+
 fn main() {
     let a = 1.0;
     let b = 2.0;
