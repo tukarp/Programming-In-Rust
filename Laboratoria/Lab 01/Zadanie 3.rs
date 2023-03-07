82b30ce38cd9ed66ad08e058e10144f0f5f20987
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-07 20:34:04 +0100

    Create Zadanie 3.rs

---
+++ Laboratoria/Lab 01/Zadanie 3.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let celsius_temperature: f32 = 15.0;
+    let fahrenheit_temperature: f32 = 32.0 + ((9.0 / 5.0) * celsius_temperature);
+    println!("Temperatura w Fahrenheitach wynosi: {fahrenheit_temperature}");
+}
