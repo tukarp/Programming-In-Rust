55f2d703c653911d8a8e54e24504f1f518929f30
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-07 20:36:16 +0100

    Create Zadanie 4.rs

---
+++ Laboratoria/Lab 01/Zadanie 4.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let fahrenheit_temperature: f32 = 60.0;
+    let celsius_temperature: f32 = fahrenheit_temperature - (32.0 * (5.0 / 9.0));
+    println!("Temperatura w Celsjuszach wynosi: {celsius_temperature}");
+}
