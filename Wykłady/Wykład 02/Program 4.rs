369924b1df62953ab939a16bbfffd4cd872925ee
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-11 22:43:49 +0100

    Create

---
+++ Wykłady/Wykład 02/Program 4.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let a = 10;
+    let b = 20;
+
+    if a > b {
+        println!("większe");
+    } else if a < b {
+        println!("mniejsze");
+    } else {
+        println!("równe");
+    }
+}
