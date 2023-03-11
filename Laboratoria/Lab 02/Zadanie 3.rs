7d8932e4c2a96af062195bb8db8c0ad2aa5449d6
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-11 19:52:16 +0100

    Update Zadanie 3.rs

--- Laboratoria/Lab 02/Zadanie 3.rs
+++ Laboratoria/Lab 02/Zadanie 3.rs
@@ -1,5 +1,11 @@
+// zamień celsjusze na fahrenheity
+fn celsius_to_fahrenheit(celsius_temperature: f32) -> f32 {
+    let fahrenheit_temperature: f32 = ((9.0 / 5.0) * celsius_temperature) + 32.0;
+    return fahrenheit_temperature;
+}
+
 fn main() {
     let celsius_temperature: f32 = 15.0;
-    let fahrenheit_temperature: f32 = ((9.0 / 5.0) * celsius_temperature) + 32.0;
-    println!("Temperatura w Fahrenheitach wynosi: {fahrenheit_temperature}");
+    let fahrenheit_temperature: f32 = celsius_to_fahrenheit(celsius_temperature);
+    println!("Temperatura {celsius_temperature} stopni Celsjusza to {fahrenheit_temperature} stopni Fahrenheita!");
 }
