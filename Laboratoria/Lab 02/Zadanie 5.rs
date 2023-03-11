4abc0443155133f5bbcfd72fa2d38468a8da9561
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-11 19:59:44 +0100

    Update Zadanie 5.rs

--- Laboratoria/Lab 02/Zadanie 5.rs
+++ Laboratoria/Lab 02/Zadanie 5.rs
@@ -1,18 +1,12 @@
-fn main() {
-    // startowa godzina
-    let mut hours_start: i32 = 10;
-    let mut minutes_start: i32 = 45;
-    let mut seconds_start: i32 = 39;
-    // godzina od której odejmujemy
-    let hours_subtract: i32 = 16;
-    let minutes_subtract: i32 = 15;
-    let seconds_subtract: i32 = 47;
-    
-    println!("Startowa godzina: {hours_start}:{minutes_start}:{seconds_start}, odejmowana godzina: {hours_subtract}:{minutes_subtract}:{seconds_subtract}");
+// oblicz różnice czasu
+fn time_difference(mut hours_start: i32, mut minutes_start: i32, mut seconds_start: i32, hours_subtract: i32, minutes_subtract: i32, seconds_subtract : i32) {
+    println!("Startowa godzina:     {hours_start}:{minutes_start}:{seconds_start}");
+    println!("Odejmowana godzina:   {hours_subtract}:{minutes_subtract}:{seconds_subtract}");
     
     hours_start -= hours_subtract;
     minutes_start -= minutes_subtract;
     seconds_start -= seconds_subtract;
+    
     if hours_start < 0 {
             hours_start *= -1;
     } if minutes_start < 0 {
@@ -20,5 +14,19 @@ fn main() {
     } if seconds_start < 0 {
             seconds_start *= -1;
     }
+    
     println!("Różnica czasu: {hours_start} godziny, {minutes_start} minut, {seconds_start} sekund");
+}
+
+fn main() {
+    // startowa godzina
+    let hours_start: i32 = 10;
+    let minutes_start: i32 = 45;
+    let seconds_start: i32 = 39;
+    // godzina od której odejmujemy
+    let hours_subtract: i32 = 16;
+    let minutes_subtract: i32 = 15;
+    let seconds_subtract: i32 = 47;
+    
+    time_difference(hours_start, minutes_start, seconds_start, hours_subtract, minutes_subtract, seconds_subtract);
 }
