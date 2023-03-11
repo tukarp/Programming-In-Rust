2d4f8746884d4f79437dea2bda34c0e01900d3fe
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-11 19:42:50 +0100

    Update Zadanie 1.rs

--- Laboratoria/Lab 02/Zadanie 1.rs
+++ Laboratoria/Lab 02/Zadanie 1.rs
@@ -1,9 +1,12 @@
+fn leap_year_check(year: i32) {
+    if((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) {
+        println!("This year is leap!");
+    } else {
+        println!("This year isn't leap!");
+    }
+}
+
 fn main() {
     let year = 2023;
-    
-    if((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) {
-        println!("Podany rok jest przystępny!");
-    } else {
-        println!("Podany rok nie jest przystępny!");
-    }
+    leap_year_check(year);
 }
