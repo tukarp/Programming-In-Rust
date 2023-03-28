25d2a30ab67895203eb36a4e987ad91770a54f20
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-28 14:31:11 +0200

    Create Program 01.rs

---
+++ Wykłady/Wykład 04/Program 01.rs
@@ -0,0 +1,11 @@
+fn wyswietl(t: &[i32]) {
+    println!("{:?}", t);
+}
+
+fn main() {
+    let mut tablica = [10, 20, 40, 100];
+    wyswietl(&tablica);
+    println!("{}", tablica[1]);
+    tablica[2] = 567;
+//     tablica[100] = 987;  // błąd -- na poziomie kompilacji
+}
