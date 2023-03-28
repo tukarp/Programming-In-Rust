5e1a787b2218a409f4740e0bc6bcf779337562c1
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-28 14:31:40 +0200

    Create Program 03.rs

---
+++ Wykłady/Wykład 04/Program 03.rs
@@ -0,0 +1,19 @@
+fn wyswietl(t: &[i32]) {
+    println!("{:?}", t);
+}
+
+fn wyswietl_jeden(t: &[i32], i: usize) {
+    let x = t.get(i);
+    println!("{:?}, {:?}, {:?}", x, x.is_none(), x.unwrap_or(&999));
+    println!("{:?}", x.unwrap());
+}
+
+fn main() {
+    let mut tablica = [10, 20, 40, 100];
+    wyswietl(&tablica);
+    println!("{}", tablica[1]);
+    tablica[2] = 567;
+    println!("{}", tablica.len());
+    wyswietl_jeden(&tablica, 1);
+//     wyswietl_jeden(&tablica, 100);  // błąd -- w czasie wykonania
+}
