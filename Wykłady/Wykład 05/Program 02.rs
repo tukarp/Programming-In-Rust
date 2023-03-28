45bb8cdf68567ce2f65959d69d0f22ca3a96817e
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-28 14:36:52 +0200

    Create Program 02.rs

---
+++ Wykłady/Wykład 05/Program 02.rs
@@ -0,0 +1,13 @@
+// fn f1() -> i32 {}
+// 
+// fn f2() -> Result<i32, BladWejsciaWyjscia> {}
+// 
+// fn f3() -> Result<i32, BladParsowania> {}
+// 
+// fn f4() -> Option<i32> {}
+
+// fn f1(...) -> Option<()>
+// // None, Some(())
+// 
+// fn f2(...) -> Result<(), JakisTypBledu>
+// // Err(.....), Ok(())
