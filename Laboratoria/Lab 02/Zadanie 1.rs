12fa40fbcca8612bd4b3ec51e41d934516b7d984
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-28 23:28:10 +0200

    Update Zadanie 1.rs

--- Laboratoria/Lab 02/Zadanie 1.rs
+++ Laboratoria/Lab 02/Zadanie 1.rs
@@ -1,4 +1,4 @@
-// sprawdź czy rok jest przystępny
+// Sprawdź czy rok jest przystępny
 fn leap_year_check(year: i32) {
     if((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) {
         println!("This year is leap!");
