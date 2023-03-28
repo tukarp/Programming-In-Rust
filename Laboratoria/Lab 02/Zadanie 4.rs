b652704b27af263f641e804205bfeeddf37f99be
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-28 23:29:01 +0200

    Update Zadanie 4.rs

--- Laboratoria/Lab 02/Zadanie 4.rs
+++ Laboratoria/Lab 02/Zadanie 4.rs
@@ -1,4 +1,4 @@
-// zamień fahrenheity na celsjusze
+// Zamień fahrenheity na celsjusze
 fn fahrenheit_to_celsius(fahrenheit_temperature: f32) -> f32 {
     let celsius_temperature: f32 = (fahrenheit_temperature - 32.0) * (5.0 / 9.0);
     return celsius_temperature;
